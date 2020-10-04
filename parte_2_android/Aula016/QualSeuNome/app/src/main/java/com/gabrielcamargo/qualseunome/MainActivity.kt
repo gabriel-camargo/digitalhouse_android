3cd2db5b61b7414c9039e538390e8e30a9d67e69
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2020-10-04 11:20:57 -0300

    Aula 016: Exercício 1 - Qual seu nome?

---
+++ parte_2_android/Aula016/QualSeuNome/app/src/main/java/com/gabrielcamargo/qualseunome/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.gabrielcamargo.qualseunome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnExibir.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v) {
+            btnExibir -> definirNome()
+        }
+    }
+
+    fun definirNome() {
+        txtNomeResposta.text = edtNome.text
+    }
+}
