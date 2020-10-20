2623d88248e8317fd983491b5117cc850df27b52
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2020-10-20 20:59:13 -0300

    Rick app - listagem

--- parte_2_android/Aula022/exercicios/RickApp/app/src/main/java/com/digitalhouse/rickapp/MainActivity.kt
+++ parte_2_android/Aula022/exercicios/RickApp/app/src/main/java/com/digitalhouse/rickapp/MainActivity.kt
@@ -2,24 +2,36 @@ package com.digitalhouse.rickapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.digitalhouse.rickapp.api.IRespostaDaApi
 import com.digitalhouse.rickapp.api.Personagem
 import com.digitalhouse.rickapp.api.RickApi
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewManager = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.cardList)
+
 
         RickApi.getData(this, object: IRespostaDaApi {
             override fun obtevePersonagens(personagens: List<Personagem>) {
-                Picasso.get()
-                        .load(personagens[1].imagemUrl)
-                        .into(minhaImagem)
+                val viewAdapter = CardRickAdapter(personagens)
+
+                recyclerView.apply {
+                    setHasFixedSize(true) //dizer que os elementos não são dinamicos
+
+                    layoutManager = viewManager
+                    adapter = viewAdapter
+                    addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                }
             }
         })
+
+
     }
 }
