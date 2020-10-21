dfe8a060f86a2b1495df5c54d8c9307f8c87a7bd
Gabriel Camargo <gabriel.rodrigocam@gmail.com>
2020-10-20 21:17:08 -0300

    Aula 022: Rick app grid

---
+++ parte_2_android/Aula022/exercicios/RickAppGrid/app/src/main/java/com/digitalhouse/rickapp/CardRickAdapter.kt
@@ -0,0 +1,43 @@
+package com.digitalhouse.rickapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.digitalhouse.rickapp.api.Personagem
+import com.squareup.picasso.Picasso
+
+class CardRickAdapter(private val dataSet: List<Personagem>):
+    RecyclerView.Adapter<CardRickAdapter.MyViewHolder>() {
+
+    //mapeia os elementos xml
+    class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val imgView: ImageView = view.findViewById(R.id.imagem)
+
+        fun bind(personagem: Personagem) {
+            Picasso.get()
+                .load(personagem.imagemUrl)
+                .into(imgView)
+        }
+    }
+
+    //infla o xml (a linha)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CardRickAdapter.MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rick_item,parent,false)
+
+        return MyViewHolder(view)
+    }
+
+    // chamado a cada item do dataset
+    override fun onBindViewHolder(holder: CardRickAdapter.MyViewHolder, position: Int) {
+        holder.bind(dataSet[position])
+    }
+
+    // qtd de itens do dataset
+    override fun getItemCount()=dataSet.size
+}
---
+++ parte_2_android/Aula022/exercicios/RickAppGrid/app/src/main/java/com/digitalhouse/rickapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.digitalhouse.rickapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.digitalhouse.rickapp.api.IRespostaDaApi
+import com.digitalhouse.rickapp.api.Personagem
+import com.digitalhouse.rickapp.api.RickApi
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewManager = GridLayoutManager(this,2)
+        val recyclerView = findViewById<RecyclerView>(R.id.cardList)
+
+
+        RickApi.getData(this, object: IRespostaDaApi {
+            override fun obtevePersonagens(personagens: List<Personagem>) {
+                val viewAdapter = CardRickAdapter(personagens)
+
+                recyclerView.apply {
+                    setHasFixedSize(true) //dizer que os elementos não são dinamicos
+
+                    layoutManager = viewManager
+                    adapter = viewAdapter
+//                    addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+                }
+            }
+        })
+
+
+    }
+}
